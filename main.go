ae2c36ef6f83c41eb033ecb5afd58412778b3e12
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-04-11 17:23:09 +0200

    s/Http/HTTP/

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 var items []help.Item
 var articleTmpl *template.Template
 
-func setHttpContentType(w http.ResponseWriter) {
+func setHTTPContentType(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 }
 
@@ -29,14 +29,14 @@ func getItem(path string) *help.Item {
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	slug := r.URL.Path[1:]
 	if slug == "" {
-		setHttpContentType(w)
+		setHTTPContentType(w)
 		// TODO: render mainpage
 	} else if slug[len(slug)-4:] == ".css" {
 		file := path.Base(slug)
 		name := file[:len(file)-4]
 		http.ServeFile(w, r, path.Join("views", name, name+".css"))
 	} else {
-		setHttpContentType(w)
+		setHTTPContentType(w)
 
 		item := getItem("help/" + slug + ".md")
 		if item == nil {
